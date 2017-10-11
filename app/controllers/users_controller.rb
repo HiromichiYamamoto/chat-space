3d56464a13d2621ef4c3fdcc6385072740166725
HiromichiYamamoto <fig@Fig.local>
2017-10-11 15:25:54 +0900

    ユーザー編集機能 サインイン、サインアウト機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
+   before_action :set_user, only: [:show, :edit, :update]
 
   def edit
   end
 
   def update
-
+    @user.update(user_params)
+    redirect_to :root
   end
 
   private
+
+  def set_user
+     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 
 end
