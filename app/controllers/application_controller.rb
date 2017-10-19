6f9950ebe8907f31237ddafeaff8edd131f96b24
HiromichiYamamoto <fig@Fig.local>
2017-10-19 17:16:41 +0900

    modify-view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  helper_method :group_message
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
@@ -11,4 +12,12 @@ class ApplicationController < ActionController::Base
     new_user_session_path
   end
 
+  def group_message(group)
+    message = Message.where(group_id: group).last
+    if (message.body)
+      message.body
+    else
+      "ありません"
+    end
+  end
 end
