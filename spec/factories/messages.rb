b9ff74680d7137f785ee63273534bb3d70a74978
HiromichiYamamoto <fig@Fig.local>
2017-10-25 12:19:58 +0900

    modify-model-tag

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
-    id          "1"
-    body        "こんにちわ"
+    id          Faker::Number.unique.number(1)
+    body        Faker::Lorem.characters
     image       File.open(File.join(Rails.root, 'spec/fixtures/image.jpg'))
     user
     group
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,6 @@ FactoryGirl.define do
   factory :user do
     name                  Faker::Name.first_name
     email                 Faker::Internet.email
-    # password              pass
-    # password_confirmation pass
     password              Faker::Internet.password(8)
   end
 end
